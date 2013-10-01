b3a65fdd7c7a436f7e42c69a4d96395d3d0ab306
Karan Tamhane <karan.tamhane@gmail.com>
2013-10-01 19:09:51 -0400

    Frequency of letters and word scoring Letters selection (for letter set) now depends on weight of letter Word score now depends upon length of word

--- word_game.rb
+++ word_game.rb
@@ -1,22 +1,32 @@
 #In Word game, the users gets a random set of letters and then constructs valid english words out of the given set.
 
-#Proposed game flow - 
-#*Choose random letters with a third of them being vowels and store them in a hash
-#*Store letters in a list (game is small enough to use list and avoid hash overhead)
-#*Display set for user
-#*Accept words from user
-#*Check for validity
-#*If valid, remove used letters from set and display rest of the letters
-#*Score words based on letters used (through scrabble letter values stored in a hash) and length of words
-#*Run till user quits. Give user option to play a new game.
-
-LETTERS_SCORE = #letter => score value
-
-CONSONANTS = 'bcdfghjklmnpqrstvwxyz'.split('')
-VOWELS = 'aeiou'.split('')
 
 LETTER_SCORE = {'a'=>1, 'j'=>8, 's'=>1,'b'=>3, 'k'=>5, 't'=>1, 'c'=>3, 'l'=>1, 'u'=>1, 'd'=>2, 'm'=>3, 'v'=>4, 'e'=>1, 
 				'n'=>1, 'w'=>4, 'f'=>4, 'o'=>1, 'x'=>8, 'g'=>2, 'p'=>3, 'y'=>4, 'h'=>4, 'q'=>10, 'z'=>10, 'i'=>1, 'r'=>1}
+
+def generate_initial_alphabet_set
+	letter_frequency = {'a'=>9, 'j'=>1, 's'=>4, 'b'=>2, 'k'=>1, 't'=>6, 'c'=>2, 'l'=>4, 'u'=>4, 'd'=>4, 'm'=>2, 'v'=>2, 
+				'e'=>12, 'n'=>6, 'w'=>2, 'f'=>2, 'o'=>8, 'x'=>1, 'g'=>3, 'p'=>2, 'y'=>2, 'h'=>2, 'q'=>1, 'z'=>1, 'i'=>9, 'r'=>6}
+	final_cons = []
+	final_vows = []
+	cons = 'bcdfghjklmnpqrstvwxyz'.split('')
+	cons.each do |letter|
+		for i in 0...letter_frequency[letter]
+			final_cons << letter
+		end
+	end
+	vows = 'aeiou'.split('')
+	vows.each do |letter|
+		for i in 0...letter_frequency[letter]
+			final_vows << letter
+		end
+	end
+	final_cons.shuffle!
+	final_vows.shuffle!
+	return final_cons, final_vows
+end
+
+CONSONANTS, VOWELS = generate_initial_alphabet_set
 
 def load_words(filename)
 	puts 'Loading wordlist...'
@@ -29,7 +39,6 @@ def load_words(filename)
 	wordlist
 end
 
-#TODO - add frequency with weighting for letters
 def generate_letter_set(num_letters)
 	letter_set = []
 	num_vowels = num_letters/3
@@ -39,13 +48,15 @@ def generate_letter_set(num_letters)
 	for j in 0...(num_letters-num_vowels)
 		letter_set << CONSONANTS.sample
 	end
-	return letter_set.sort
+	letter_set.sort
 end
+
 
 def is_word_valid?(word, wordlist)
 	return true if wordlist.include? word
 	false
 end
+
 
 def are_letters_available?(letter_set, word)
 	word.split('').each do |letter|
@@ -59,14 +70,22 @@ def score_word(word)
 	word.split('').each do |letter|
 		score += LETTER_SCORE[letter]
 	end
-	score
+	if word.length <= 3
+		score
+	elsif word.length <=6
+		score*2
+	elsif word.length > 6
+		score*3
+	end
 end
+
 
 def play_game
 	wordlist = load_words('words.txt')
 	num_letters = 10
 	letter_set = generate_letter_set(num_letters)
-	puts 'Welcome to Word Game! Press . anytime to exit.'
+	puts 'Welcome to Boggle! Press . anytime to exit.'
+	puts
 	total_score = 0
 	while letter_set.length > 0
 		puts "Available letters: ", letter_set.join(' ')
@@ -86,7 +105,6 @@ def play_game
 		end 
 		for i in 0...word.length
 			letter_index = letter_set.index(word[i])
-			#print letter_index
 			letter_set.delete_at letter_index
 		end
 		word_score = score_word(word)
@@ -95,7 +113,7 @@ def play_game
 		puts "Your total score is #{total_score}"
 		puts ''
 	end
-	puts 'Thank you for playing Boggle! Goodbye!'
+	puts 'Thank you for playing Boggle! Goodbye!' if word != '.'
 end
 
 
