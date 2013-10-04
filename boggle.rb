400a15f2cc10a8883087980b128b47f10d403623
Karan Tamhane <karan.tamhane@gmail.com>
2013-10-04 12:35:24 -0400

    Board creation successful! (iteration 1)

--- boggle.rb
+++ boggle.rb
@@ -2,7 +2,7 @@
 
 # TIPS
 # Possible way to check for word board path --> precompute all possible words in board and just lookup every time
-# Graph search - avoid cycles
+# graph search - avoid cycles
 require File.expand_path('word_game_for_boggle.rb')
 
 class Letter
@@ -10,24 +10,40 @@ class Letter
 		@letter = letter
 		@row = row
 		@column = column
+		@up = nil
+		@down = nil
+		@left = nil
+		@right = nil
+		@lup = nil
+		@rup = nil
+		@ldown = nil
+		@rdown = nil
 	end
-	attr_accessor :letter, :row, :column
+	attr_accessor :letter, :row, :column, :up, :down, :left, :right, :lup, :rup, :ldown, :rdown
 end
 
 def make_boggle_board(letter_set, board_size)
-	graph = {}
+	board = {}
 	for i in 0...letter_set.length
 		letter_obj = Letter.new(letter_set[i], i/board_size, i%board_size)
-		graph[letter_obj] = []
-		for neighbor in graph.keys
-			if (neighbor.column == letter_obj.column-1 && neighbor.row == letter_obj.row) || (neighbor.column == letter_obj.column-1 && neighbor.row == letter_obj.row-1) || (neighbor.column == letter_obj.column && neighbor.row == letter_obj.row-1) || (neighbor.column == letter_obj.column+1 && neighbor.row == letter_obj.row+1)
-
-				graph[neighbor] << letter_obj
-				graph[letter_obj] << neighbor
+		board[letter_obj] = 1
+		for neighbor in board.keys
+			if (neighbor.column == letter_obj.column-1 && neighbor.row == letter_obj.row)
+				letter_obj.left = [neighbor.letter, neighbor]
+				neighbor.right = [letter_obj.letter, letter_obj]
+			elsif (neighbor.column == letter_obj.column-1 && neighbor.row == letter_obj.row-1)
+				letter_obj.lup = [neighbor.letter, neighbor]
+				neighbor.rdown = [letter_obj.letter, letter_obj]
+			elsif (neighbor.column == letter_obj.column && neighbor.row == letter_obj.row-1)
+				letter_obj.up = [neighbor.letter, neighbor]
+				neighbor.down = [letter_obj.letter, letter_obj]
+			elsif (neighbor.column == letter_obj.column+1 && neighbor.row == letter_obj.row+1)
+				letter_obj.rup = [neighbor.letter, neighbor]
+				neighbor.ldown = [letter_obj.letter, letter_obj]
 			end
 		end
 	end
-	graph
+	board
 end
 
 def display_board(letter_set, board_size)
@@ -42,13 +58,44 @@ def display_board(letter_set, board_size)
 end
 
 def word_path_valid_on_board?(word, board)
+	#Try and think of a different approach!!!!!!!!!
+	starting_letter = []
+	letter_index = 0
+	board.keys.each do |letter_obj|
+		if i == 0 && letter_obj.letter == word[i]
+			starting_letter << letter_obj
+		end
+	end
+	letter_index += 1
 	true
+end
+
+def display_objects_on_board(board)
+	start = nil
+	count = 0
+	row = 0
+	while count < board.length
+		for obj in board.keys
+			if obj.row == row && obj.column == 0
+				start = obj
+				break
+			end
+		end
+		for i in 0...Math.sqrt(board.length)
+			count += 1
+			print start.letter,' '
+			start = start.right[1] if start.right
+		end
+		puts
+		row += 1
+	end
 end
 
 def play_boggle
 	wordlist = load_words('words.txt')
 	word_score = total_score = 0
 	puts
+	#TODO - Assert presence of number for board
 	print 'Enter size of Boggle board required: '
 	board_size = gets.chomp.to_i
 	letter_set = generate_letter_set(board_size*board_size)
