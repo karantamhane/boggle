8adb51992f20e8e5dec1c2fd1e12764cd1f250d0
Karan Tamhane <karan.tamhane@gmail.com>
2013-10-04 17:24:50 -0400

    First working version of Boggle Very very buggy!

--- boggle.rb
+++ boggle.rb
@@ -18,8 +18,9 @@ class Letter
 		@rup = nil
 		@ldown = nil
 		@rdown = nil
+		@visited = false
 	end
-	attr_accessor :letter, :row, :column, :up, :down, :left, :right, :lup, :rup, :ldown, :rdown
+	attr_accessor :letter, :row, :column, :up, :down, :left, :right, :lup, :rup, :ldown, :rdown, :visited
 end
 
 def make_boggle_board(letter_set, board_size)
@@ -37,7 +38,7 @@ def make_boggle_board(letter_set, board_size)
 			elsif (neighbor.column == letter_obj.column && neighbor.row == letter_obj.row-1)
 				letter_obj.up = [neighbor.letter, neighbor]
 				neighbor.down = [letter_obj.letter, letter_obj]
-			elsif (neighbor.column == letter_obj.column+1 && neighbor.row == letter_obj.row+1)
+			elsif (neighbor.column == letter_obj.column+1 && neighbor.row == letter_obj.row-1)
 				letter_obj.rup = [neighbor.letter, neighbor]
 				neighbor.ldown = [letter_obj.letter, letter_obj]
 			end
@@ -58,16 +59,58 @@ def display_board(letter_set, board_size)
 end
 
 def word_path_valid_on_board?(word, board)
-	#Try and think of a different approach!!!!!!!!!
-	starting_letter = []
-	letter_index = 0
+	#Try recursion to take into account duplicate letters (second, third, fourth letters can also have adjacent duplicates! not just first letters.)
+	#TODO - keep track of already formed words
+	first_letter_list = []
 	board.keys.each do |letter_obj|
-		if i == 0 && letter_obj.letter == word[i]
-			starting_letter << letter_obj
+		if letter_obj.letter == word[0]
+			first_letter_list << letter_obj
 		end
 	end
-	letter_index += 1
-	true
+	first_letter_list_index = 0
+	board_size = Math.sqrt(board.keys.length)
+	while first_letter_list_index < first_letter_list.length
+		letter_found = true
+		word_found = false
+		word_letter_index = 1
+		first_letter = first_letter_list[first_letter_list_index]
+		first_letter.visited = true
+		while word_letter_index < word.length
+			puts first_letter.letter
+			if first_letter.up && first_letter.up[0] == word[word_letter_index] && first_letter.up[1].visited == false
+				first_letter = first_letter.up[1]
+			elsif first_letter.down && first_letter.down[0] == word[word_letter_index] && first_letter.down[1].visited == false
+				first_letter = first_letter.down[1]
+			elsif first_letter.left && first_letter.left[0] == word[word_letter_index] && first_letter.left[1].visited == false
+				first_letter = first_letter.left[1]
+			elsif first_letter.right && first_letter.right[0] == word[word_letter_index] && first_letter.right[1].visited == false
+				first_letter = first_letter.right[1]
+			elsif first_letter.lup && first_letter.lup[0] == word[word_letter_index] && first_letter.lup[1].visited == false
+				first_letter = first_letter.lup[1]
+			elsif first_letter.rup && first_letter.rup[0] == word[word_letter_index] && first_letter.rup[1].visited == false
+				first_letter = first_letter.rup[1]
+			elsif first_letter.ldown && first_letter.ldown[0] == word[word_letter_index] && first_letter.ldown[1].visited == false
+				first_letter = first_letter.ldown[1]
+			elsif first_letter.rdown && first_letter.rdown[0] == word[word_letter_index] && first_letter.rdown[1].visited == false
+				first_letter = first_letter.rdown[1]
+			else
+				letter_found = false
+				puts 'letter not found!'
+				break
+			end
+			first_letter.visited = true
+			word_letter_index += 1
+		end
+		if letter_found
+			word_found = true
+		end
+		first_letter_list_index += 1
+		#Reset visited nodes to not visited
+		for letter_obj in board.keys
+			letter_obj.visited = false
+		end
+	end
+	word_found
 end
 
 def display_objects_on_board(board)
@@ -92,12 +135,17 @@ def display_objects_on_board(board)
 end
 
 def play_boggle
+	puts 'Welcome to Boggle! Press . anytime to exit'
+	puts
 	wordlist = load_words('words.txt')
 	word_score = total_score = 0
 	puts
-	#TODO - Assert presence of number for board
+	#Assert presence of number for board
 	print 'Enter size of Boggle board required: '
 	board_size = gets.chomp.to_i
+	until board_size.kind_of?(Integer)
+		puts 'Please enter an integer!'
+	end
 	letter_set = generate_letter_set(board_size*board_size)
 	board = make_boggle_board(letter_set, board_size)
 	while true
@@ -118,7 +166,6 @@ def play_boggle
 			puts "A word is valid only if it traces a pattern on the current board! Please try again."
 			next
 		end
-		#TODO - Check if word path exists on board
 		word_score = score_word(word)
 		total_score += word_score if word_score
 		puts "You scored #{word_score} points for #{word}."
@@ -127,4 +174,3 @@ def play_boggle
 	end
 	puts 'Thank you for playing Boggle. Goodbye!'
 end
-
